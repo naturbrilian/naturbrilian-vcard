911f349edf78d9184226db5289e3cae5d8181f8c
Lieba Natur Brilian <gingalibadeidara@gmail.com>
2025-04-20 12:50:19 +0700

    Update coding-stats.tsx

--- apps/web/src/components/about/coding-stats.tsx
+++ apps/web/src/components/about/coding-stats.tsx
@@ -69,7 +69,7 @@ function CodingStats({ techStacks, githubUsername }: CodingStatsProps) {
               <div className="flex items-center gap-2 text-white-2 mt-4 ml-4">
                 <LuMapPin size={18} />
                 <h2 className="text-sm font-light">
-                  Taipei, Taiwan (UTC +08:00)
+                  Kediri, East Java (UTC +07:00)
                 </h2>
               </div>
               <Globe />
